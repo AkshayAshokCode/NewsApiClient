bf54988f651af774ec4dca5901edbc7f74445e2a
Riderpool1 <akshayashokan1054.com>
2022-01-25 14:55:54 +0530

    recyclerview with diffUtils

--- app/src/main/java/com/akshayashokcode/newsapiclient/presentation/adapter/NewsAdapter.kt
+++ app/src/main/java/com/akshayashokcode/newsapiclient/presentation/adapter/NewsAdapter.kt
@@ -1,24 +1,57 @@
 package com.akshayashokcode.newsapiclient.presentation.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.akshayashokcode.newsapiclient.data.model.Article
 import com.akshayashokcode.newsapiclient.databinding.NewsListItemBinding
 import com.bumptech.glide.Glide
 
-class NewsAdapter {
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
-    inner class newsViewHolder(
-        val binding:NewsListItemBinding
-    ):RecyclerView.ViewHolder(binding.root){
+    private val callback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
 
-        fun bind(article:Article){
-            binding.tvTitle.text=article.title
-            binding.tvDescription.text=article.description
-            binding.tvPublishedAt.text=article.publishedAt
-            binding.tvSource.text=article.source.name
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    val differ=AsyncListDiffer(this,callback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+       val binding=NewsListItemBinding
+           .inflate(LayoutInflater.from(parent.context),parent,false)
+        return NewsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article=differ.currentList[position]
+        holder.bind(article)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    inner class NewsViewHolder(
+        val binding: NewsListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(article: Article) {
+            binding.tvTitle.text = article.title
+            binding.tvDescription.text = article.description
+            binding.tvPublishedAt.text = article.publishedAt
+            binding.tvSource.text = article.source.name
 
             Glide.with(binding.ivArticleImage.context).load(article.urlToImage)
                 .into(binding.ivArticleImage)
         }
     }
+
+
 }
